203e3b7224b347ee83aacc4e79dcf8c9bd251adb
YangKian <45479280+YangKian@users.noreply.github.com>
2019-07-25 21:41:25 +0800

    add description of the task

--- src/seven_kyu/Descending_Order.java
+++ src/seven_kyu/Descending_Order.java
@@ -3,6 +3,18 @@ package seven_kyu;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in
+ * descending order. Essentially, rearrange the digits to create the highest possible number.
+ *
+ * Examples:
+ * Input: 21445 Output: 54421
+ *
+ * Input: 145263 Output: 654321
+ *
+ * Input: 1254859723 Output: 9875543221
+ */
+
 public class Descending_Order {
     public static int sortDesc(final int num) {
         int temp = num, ans = 0;
